2c16c479ab0f6386cdfdb32ffc8adac838533367
vinaynepal2025-byte <vinaynepal2025-byte@users.noreply.github.com>
2026-08-01 07:36:15 +0000

    deploy: 28f8515aa3762ff8d10a03f4640350d414d4606a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "11257caedbf767bf61598eeb9f84b9a8",
-"flutter_bootstrap.js": "fe87b453a07a0dd400abb4f6e8820e74",
+"flutter_bootstrap.js": "ade62b135eaf5a175f53289756340db3",
 "icons/Icon-192.png": "0b25f6fcf2b85f386fedd00d926a8c3a",
 "icons/Icon-512.png": "e8aa82c3e58f71704b19ecf592533926",
 "icons/Icon-maskable-512.png": "e8aa82c3e58f71704b19ecf592533926",
