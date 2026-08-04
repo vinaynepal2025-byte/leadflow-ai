12b74acd7b82f2dc172bbb2e073b61b2fd65ddee
vinaynepal2025-byte <vinaynepal2025-byte@users.noreply.github.com>
2026-08-04 17:04:27 +0000

    deploy: c84a997605c37c4044e7039363e1e113d25ae7c3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3391619080"
+    serviceWorkerVersion: "2427759582"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "11257caedbf767bf61598eeb9f84b9a8",
-"flutter_bootstrap.js": "570a067d686cb23fb7675b3f3d6eead4",
+"flutter_bootstrap.js": "07e4ed864708b2659f8965a3247dea42",
 "icons/Icon-192.png": "0b25f6fcf2b85f386fedd00d926a8c3a",
 "icons/Icon-512.png": "e8aa82c3e58f71704b19ecf592533926",
 "icons/Icon-maskable-512.png": "e8aa82c3e58f71704b19ecf592533926",
