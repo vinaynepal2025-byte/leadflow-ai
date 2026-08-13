d00b4a99009359219f68517532ad22e2ceb785de
vinaynepal2025-byte <vinaynepal2025-byte@users.noreply.github.com>
2026-08-13 06:03:16 +0000

    deploy: 97f16b48872ad943428ac69f1c91552f15b0d9e6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "879052981"
+    serviceWorkerVersion: "2614777182"
   }
 });
