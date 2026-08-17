500970d07923d79885dcfae578c1e2606308ee2b
vinaynepal2025-byte <vinaynepal2025-byte@users.noreply.github.com>
2026-08-17 09:19:46 +0000

    deploy: 63f10c2259aeff10a09284738466d32dfaa5182e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3713845477"
+    serviceWorkerVersion: "259286003"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,8 +46,8 @@ const RESOURCES = {"version.json": "247081c9b8876517d0c7c9ee8613a074",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/packages/record_web/assets/js/record.fixwebmduration.js": "1f0108ea80c8951ba702ced40cf8cdce",
 "assets/packages/record_web/assets/js/record.worklet.js": "6d247986689d283b7e45ccdf7214c2ff",
-"main.dart.js": "11047e3b663e93350403d6891eb7980f",
-"flutter_bootstrap.js": "4188b7933ae0b14448cc0b77c756e73a",
+"main.dart.js": "54d2366adaa832532ed4a5f1c5dbd4ac",
+"flutter_bootstrap.js": "9b51f246eabfc01d29e5a0656c620b0d",
 "manifest.json": "26740c0e14004f4bb51588e66945acb8"};
 // The application shell files that are downloaded before a service worker can
 // start.
